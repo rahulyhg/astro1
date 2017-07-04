85528bd06fa863ce8fddba66eb534aed1c3182f9
rahul-soshte <rahul.soshte47@gmail.com>
2017-07-05 02:18:49 +0530

    Removed useless stuff

--- appointment.php
+++ appointment.php
@@ -1,22 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
 
 <div class="container">
-<?php include "header.html" ?>
-<br>
-<br>
-<br>
-<br>
+<?php include "header.php" ?>
+
   <h2>Give your details</h2>
   <form role="form">
-    <div class="form-group">
+    <div>
       <label for="name">Name:</label>
       <input type="name" class="form-control" id="name" placeholder="Name">
     </div>
@@ -49,7 +42,7 @@
       <label><input type="checkbox">Female</label>
     </div>
 
-    <button type="submit" class="btn btn-default">Submit</button>
+    <button type="submit">Submit</button>
   </form>
 </div>
 </body> 
