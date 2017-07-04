6b9eeec14da58d44fc0b0332338c6a68de3eab31
rahul-soshte <rahul.soshte47@gmail.com>
2017-07-05 02:34:23 +0530

    Removing stuff

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <body>
   <div class="list-container"
       <li><a href="index.php">Home</a></li>
-      <li><a href="appointment.php">Appointment</a></li>
+      <!--<li><a href="appointment.php">Appointment</a></li>-->
       <li><a href="contact.php">Contact</a></li>  
     </div>
 
