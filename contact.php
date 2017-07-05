565c0ed10617e696453a138a9c093412e7cf0e59
rahul-soshte <rahul.soshte47@gmail.com>
2017-07-05 16:48:20 +0530

    Added image

--- contact.php
+++ contact.php
@@ -3,11 +3,13 @@
 <head>
 </head>
 <body>
-
 <div>
 <?php include "header.php" ?>
+
 <h2>Phone Number:9833839020</h2>
 <h2>Address:A 12 Vaikunth CHS,Nahurgoan,Mulund West,Mumbai-80</h2>
+<h2>Timings:7pm-9pm and Fully Availble on Public Holidays</h2>
+<h2>Call at the given Contact for taking an Appointment</h2> 											
 </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>VijayKumar Soshte</title>  
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
-<?php include "header.php"; ?>
+<img class="img-circle" src="images/dpic.jpg">
+
 <div class="container">
   <h3>Introduction</h3>
   <p>Hello This is Vijaykumar Soshte,a Astrologer with more than 15 years of practice.Bring your problems to me I will solve them in a jiffy.</p>
@@ -14,6 +18,10 @@
 <p>The knowledge of future prompts one to render help to those who are in need,banish evil thoughts from mind and make efforts to lead a simple honest and pious life.The saying "Do your duty with sincerity and leave the result to God" holds a more forceful appeal Astrology proves that failure and success depend on your previous Karma;sufferings are due to sins committed previously and this thought helps us to be good to society.
 </p>
 
+<h2>Phone Number:9833839020</h2>
+<h2>Address:A 12 Vaikunth CHS,Nahurgoan,Mulund West,Mumbai-80</h2>
+<h2>Timings:7pm-9pm and Fully Availble on Public Holidays</h2>
+<h2>Call at the given Contact for taking an Appointment</h2> 											
 
 </div>
 </body>
